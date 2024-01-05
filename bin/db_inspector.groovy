2f26764f65640dcf101c848204755382a29d3735
pvranik <petr.vranik@mgm-tp.com>
2024-01-05 11:39:45 +0100

    no oracle

--- bin/db_inspector.groovy
+++ bin/db_inspector.groovy
@@ -1,5 +1,18 @@
 #!/usr/bin/env groovy
 
+@Grapes([
+		@Grab('org.postgresql:postgresql:42.7.1'),
+		@Grab('com.oracle.jdbc:ojdbc10:19.3.0.0'),
+		@Grab('org.slf4j:slf4j-api:2.0.10'),
+		@Grab('org.slf4j:slf4j-simple:2.0.10'),
+		@Grab('info.picocli:picocli-groovy:4.7.4'),
+		@Grab('commons-io:commons-io:2.11.0'),
+		@Grab('com.fasterxml.jackson.core:jackson-core:2.14.2'),
+		@Grab('com.fasterxml.jackson.core:jackson-databind:2.15.2'),
+		@Grab('com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.2'),
+])
+@groovy.lang.GrabConfig(systemClassLoader = true)
+
 import DbInspector
 import CommandLine
 
